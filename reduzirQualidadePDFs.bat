2444fa5b45000542ed8bf8fe3c3467a7cef4706b
Alberto Vieira Lima <37252358+avlgithub@users.noreply.github.com>
2022-06-22 12:20:35 -0300

    Add arquivos (#5)
    Exemplo para 
    DeslocaPGDoPDF  = Deslocar uma página
    JuntaPDF = Concatenar PDF´s
    reduzirQualidadePDFs = Reduz a qualidade de Saída do PDF "incluso .job de configuração"
    VerificarPageColorPB = Verifica se a página é 100% PB ou color

---
+++ reduzirQualidadePDFs.bat
@@ -0,0 +1,5 @@
+c:
+cd\
+cd GMC
+pnettc C:\util\reduzirQualidadePDF\reduzirQualidadePDFs.wfd -diretorioEntrada "C:\util\reduzirQualidadePDF\in" -e "PDF" -o Saida -f "C:\util\reduzirQualidadePDF\Out\%%h02.pdf"
+pause
---
+++ reduzirQualidadePDFs_Reduzir.bat
@@ -0,0 +1,5 @@
+c:
+cd\
+cd GMC
+pnettc C:\util\reduzirQualidadePDF\reduzirQualidadePDFs.wfd -diretorioEntrada "C:\util\reduzirQualidadePDF\in" -e "PDF" -o Saida -f "C:\util\reduzirQualidadePDF\Out\%%h02.pdf" -splitbygroup -c C:\util\reduzirQualidadePDF\72dpi.job
+pause
